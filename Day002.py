170365e12fdd8cace99bd2c5ca3b1da5b6cc6571
Irvan Hendrik <irvan.hendrik@gmail.com>
2021-11-23 18:08:59 +0000

    Day002

--- Day002.py
+++ Day002.py
@@ -4,6 +4,7 @@ tip=int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people=int(input("How many people to split the bill? "))
 bill_with_tip=((tip/100)*bill)+bill
 result = round((bill_with_tip/people),2)
+result = "{:.2f}".format(result)
 print("Each person should pay: $"+str(result))
 
 #print ((bill/people)*(1+(tip/100)))
